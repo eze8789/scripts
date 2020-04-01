025b44b268c4ffe3eb0ea818d7839d7d13152725
eze8789 <ezequiel.reyna@gmail.com>
2020-04-01 02:16:13 -0300

    First commit with Readme and first bash script to test tcp port status

---
+++ README.md
@@ -0,0 +1,5 @@
+**Bash and Python Scripts**
+
+Here you will find a collection of useful scripts and utilities I written for different purpouses.
+
+Some of them are a work in progress or could change within time. 
---
+++ tcp_port_status.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+host="$1"
+port="$2"
+
+if [ -z "$host" ] || [ -z "$port" ]; then
+  echo "usage: scan <hostname> <port>" 
+  exit 1
+fi  
+
+</dev/tcp/"$host"/"$port" && echo Port is open || echo Port is closed
